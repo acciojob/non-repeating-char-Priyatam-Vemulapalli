b4d45bc38d8a6b1aa08dae2fbddecb7b4d8d6e88
Priyatam-Vemulapalli <priyatamvemulapalli@gmail.com>
2024-10-12 15:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let freq = new Array(26).fill(0);
+    for (let c of str) {
+        freq[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+    for(let i=0;i<str.length;i++){
+         if (freq[str[i].charCodeAt(0) - 'a'.charCodeAt(0)] == 1) {
+            return str[i];
+        }
+    }
+    return null;
+	
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
